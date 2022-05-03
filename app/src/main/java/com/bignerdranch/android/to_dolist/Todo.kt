48eb8766d78f7c07c04c962e323d8f73ef7a6284
daniel-iroka <danieliroka67@gmail.com>
2022-05-03 14:57:17 +0100

    Finally fixed the gradle plugin version error by temporarily ignoring it.

--- app/src/main/java/com/bignerdranch/android/to_dolist/Todo.kt
+++ app/src/main/java/com/bignerdranch/android/to_dolist/Todo.kt
@@ -5,6 +5,10 @@ import androidx.room.PrimaryKey
 import java.util.*
 
 // Our Model class. This class will represent our database table
+
+// TODO - TOMORROW, TRY AND FIX THE GRADLE DEPENDENCY ERRORS FOR THIS PROJECT
+// TODO - FIX HERE IS TO UPDATE THE ANDROID STUDIO VERSION I HOPE
+
 @Entity
 data class Todo(@PrimaryKey val id: UUID = UUID.randomUUID(), var title: String = "", var date: Date = Date(), var time: Date = Date(),
                 var todoCheckBox : Boolean = false)
