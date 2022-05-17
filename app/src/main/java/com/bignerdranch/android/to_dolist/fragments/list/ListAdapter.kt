94e3218b275b07b9b0ea1b69672227db16c4f288
daniel-iroka <danieliroka67@gmail.com>
2022-05-17 21:28:30 +0100

    Finally fixed this 100 years Text StrikeThrough Bug.

--- app/src/main/java/com/bignerdranch/android/to_dolist/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/to_dolist/fragments/list/ListAdapter.kt
@@ -4,15 +4,18 @@ import android.annotation.SuppressLint
 import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.bignerdranch.android.to_dolist.R
 import com.bignerdranch.android.to_dolist.databinding.CustomRowBinding
 import com.bignerdranch.android.to_dolist.fragments.add.SIMPLE_DATE_FORMAT
 import com.bignerdranch.android.to_dolist.fragments.add.SIMPLE_TIME_FORMAT
 import com.bignerdranch.android.to_dolist.model.Todo
 import java.text.SimpleDateFormat
 import java.util.Locale
+
 
 class ListAdapter: Adapter<ListAdapter.TodoViewHolder>() {
     private var todoList = emptyList<Todo>()
@@ -23,7 +26,7 @@ class ListAdapter: Adapter<ListAdapter.TodoViewHolder>() {
         if (cbTask) {
             tvTaskTitle.paintFlags = tvTaskTitle.paintFlags  or STRIKE_THRU_TEXT_FLAG
         } else {
-            tvTaskTitle.paintFlags = tvTaskTitle.paintFlags or STRIKE_THRU_TEXT_FLAG.inv()
+            tvTaskTitle.paintFlags = tvTaskTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
         }
     }
 
@@ -33,7 +36,8 @@ class ListAdapter: Adapter<ListAdapter.TodoViewHolder>() {
         // this can be done in an inline variable and I may experiment on it later.
         val binding = CustomRowBinding.inflate(LayoutInflater.from(parent.context),
             parent,
-            false)
+            false
+        )
         return TodoViewHolder(binding)
 
     }
@@ -48,13 +52,11 @@ class ListAdapter: Adapter<ListAdapter.TodoViewHolder>() {
             binding.tvTaskTime.text = timeLocales.format(todo.time)
             binding.cbTask.isChecked = todo.todoCheckBox
 
-            // TODO - FIX THIS MESS WHEN I COME BACK, CHANGE THE ENTIRE IMPLEMENTATION
-            /** Our strikeThrough Implementation starts. The checkedListener here does the main job **/
-            /**toggleStrikeThrough(binding.tvTaskTitle, todo.todoCheckBox)
+            toggleStrikeThrough(binding.tvTaskTitle , todo.todoCheckBox)
             binding.cbTask.setOnCheckedChangeListener { _, isChecked ->
                 toggleStrikeThrough(binding.tvTaskTitle, isChecked)
-                // This means if the value was false, it is now true and vice versa.
-            }**/
+                todo.todoCheckBox = !todo.todoCheckBox
+            }
         }
     }
 
@@ -67,5 +69,4 @@ class ListAdapter: Adapter<ListAdapter.TodoViewHolder>() {
         // todo - This is a suppressed lint warning. Later check it online and see if there is a way to improve it.
         notifyDataSetChanged()
     }
-
 }
